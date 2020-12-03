c51868f36d2b9fc8f9c75c473c7ab6a53057f092
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-12-03 15:37:51 +0530

    added:subtraction

--- JavaScript.js
+++ JavaScript.js
@@ -2,5 +2,7 @@ let a=10;
 let b=20;
 let c=a+b;
 let d=a*b;
+let s=a-b;
 console.log("the addition of a and b is :"+c);
 console.log("the mul of a and b is :"+d);
+console.log("the subtraction of a and b :"+s);
